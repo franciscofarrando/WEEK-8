1ffd9cfb87e4b139177a68f9ee500678c537a08f
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-29 20:20:13 +0100

    ejercicio 3

---
+++ EJERCICIOS_VARIOS/E2/main.js
@@ -0,0 +1,9 @@
+let question2 = prompt("Es de IKEA? s/n");
+let question3 = prompt("Sirve para sentarse? s/n");
+let question4 = prompt("Es de madera de Roble? s/n");
+let question5 = prompt("Es de un diseñador conocido? s/n");
+let question6 = prompt("Sirve para apoyar cosas? s/n");
+
+if (question5 == "s" && question6 == "s"){
+    
+}
---
+++ EJERCICIOS_VARIOS/E3/main.js
@@ -0,0 +1,13 @@
+let comida = prompt("Pide un plato: (cuando no quieras mas pon 'salir'");
+let veces = ""
+
+while (comida != "salir"){
+    comida = prompt("Has pedido: " + comida + " ; pon 'salir' cuando no desees mas.");
+    veces++;
+    
+}
+if (veces < "4"){
+    document.write("Has pedido: " +veces+ " platos");
+}else{
+        document.write("Has pedido: " +veces+ " platos. VAS A REVENTAR");
+    }
