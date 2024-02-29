5799265b6edd5a992f83ba84f4816740abbd2455
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-29 22:17:43 +0100

    EJERCICIO 4

---
+++ EJERCICIOS_VARIOS/E4/main.js
@@ -0,0 +1,23 @@
+
+    var a = prompt("name1");
+    var b = prompt("name2");
+
+    document.write("En Barcelona está " +a+ ", y en Tarragona está " +b +"<br>");
+    
+    
+    function inverse(){
+
+        let c = a;
+        a=b;
+        b=c;
+        
+
+     document.write("En Barcelona está " +a+ ", y en Tarragona está " +b);
+    }
+    inverse();
+
+    
+   
+
+
+
