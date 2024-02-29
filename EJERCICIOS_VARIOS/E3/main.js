231c1550d4fa2b6f739c0f3b2d49584bda67bf95
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-29 20:25:25 +0100

    submit

--- EJERCICIOS_VARIOS/E3/main.js
+++ EJERCICIOS_VARIOS/E3/main.js
@@ -1,13 +1,15 @@
-let comida = prompt("Pide un plato: (cuando no quieras mas pon 'salir'");
-let veces = ""
+let comida = prompt("Pide un plato: (cuando no quieras mas pon 'salir')");
+let veces = "0"
 
 while (comida != "salir"){
     comida = prompt("Has pedido: " + comida + " ; pon 'salir' cuando no desees mas.");
     veces++;
     
 }
+
 if (veces < "4"){
     document.write("Has pedido: " +veces+ " platos");
+    
 }else{
         document.write("Has pedido: " +veces+ " platos. VAS A REVENTAR");
     }
